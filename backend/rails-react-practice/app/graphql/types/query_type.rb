7a85cb42d091f774710f2ca85180d5111eef2750
terakura.aina <aicaierwei@gmail.com>
2023-02-11 10:36:57 +0900

    add graphql

---
+++ backend/rails-react-practice/app/graphql/types/query_type.rb
@@ -0,0 +1,22 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+
+    field :current_date, String, null: false, description: '今日の日付'
+    # TODO: remove me
+    field :test_field, String, null: false,
+      description: "An example field added by the generator"
+    def test_field
+      "Hello World!"
+    end
+
+    def current_date
+      Time.current.strftime("%Y年 %m月 %d日")
+    end
+  end
+end
